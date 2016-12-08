bc1b601040e7417b17cc8964a51d13c19f6406af
宫迪 <gongdi@gongdideMacBook-Pro.local>
2016-12-08 14:25:24 +0800

    -xzc -sb

---
+++ gd123/gd123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gd123
+//
+//  Created by 宫迪 on 2016/12/8.
+//  Copyright © 2016年 gd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
